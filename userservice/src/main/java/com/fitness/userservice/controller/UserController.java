2b1452774c6c77806039ed0446fbc69eb2587da7
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-04-06 11:00:24 +0530

    Add Web Client

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -29,4 +29,10 @@ public class UserController {
         return ResponseEntity.ok(userService.register(request));
     }
 
+    @GetMapping("/{userId}/validate")
+    public ResponseEntity<Boolean> validateUser(@PathVariable String userId)
+    {
+        return ResponseEntity.ok(userService.existsByUserId(userId));
+    }
+
 }
