4ab265ab0fb3d3b80fb41cff2dc80a09fd106c7d
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-04-13 12:41:41 +0530

    add frontend project

---
+++ fitness-app-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router,Navigate,Route,Routes,useLocation} from 'react-router';
+import Button from '../node_modules/@mui/material/esm/Button/Button';
+
+function App() {
+  
+
+  return (
+    <Router>
+      <Button variant='contained' color='#dc004e'>Login</Button>
+    </Router>
+  )
+}
+
+export default App
